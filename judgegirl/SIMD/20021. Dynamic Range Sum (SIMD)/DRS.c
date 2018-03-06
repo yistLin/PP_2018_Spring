21abef07478917420ed20457785334854255dee4
yistLin <yishen992@gmail.com>
2018-03-06 13:37:51 +0800

    reduce computations

--- judgegirl/SIMD/20021. Dynamic Range Sum (SIMD)/DRS.c
+++ judgegirl/SIMD/20021. Dynamic Range Sum (SIMD)/DRS.c
@@ -14,14 +14,10 @@ int32_t search_range(Rect rect, int32_t x[], int32_t y[], int32_t w[], int32_t n
         vec_x = _mm256_loadu_si256((__m256i const *)(x+i));
         vec_y = _mm256_loadu_si256((__m256i const *)(y+i));
 
-        res = _mm256_and_si256(_mm256_and_si256(_mm256_or_si256(_mm256_cmpgt_epi32(vec_x, lx),
-                                                                _mm256_cmpeq_epi32(vec_x, lx)),
-                                                _mm256_or_si256(_mm256_cmpgt_epi32(rx, vec_x),
-                                                                _mm256_cmpeq_epi32(rx, vec_x))),
-                               _mm256_and_si256(_mm256_or_si256(_mm256_cmpgt_epi32(vec_y, ly),
-                                                                _mm256_cmpeq_epi32(vec_y, ly)),
-                                                _mm256_or_si256(_mm256_cmpgt_epi32(ry, vec_y),
-                                                                _mm256_cmpeq_epi32(ry, vec_y))));
+        res = _mm256_and_si256(_mm256_and_si256(~_mm256_cmpgt_epi32(lx, vec_x),
+                                                ~_mm256_cmpgt_epi32(vec_x, rx)),
+                               _mm256_and_si256(~_mm256_cmpgt_epi32(ly, vec_y),
+                                                ~_mm256_cmpgt_epi32(vec_y, ry)));
 
         res_int = (int32_t*)&res;
         for (int j = 0; j < 8 && (i+j) < n; j++) {
