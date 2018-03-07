1407bf773cca93b89524ced86c0c5dd0632cbde9
yistLin <yishen992@gmail.com>
2018-03-07 23:54:53 +0800

    10022 solved

--- judgegirl/OpenMP & Pthread/10022. Fast Matrix Multiplication (OpenMP)/matrix.c
+++ judgegirl/OpenMP & Pthread/10022. Fast Matrix Multiplication (OpenMP)/matrix.c
@@ -1,18 +1,26 @@
-#include <stdlib.h>
+#include <omp.h>
 
 #include "matrix.h"
 
-/* Original
- * 3.in  1.84s user 0.01s system 99% cpu 1.865 total
- * 4.in  9.89s user 0.05s system 99% cpu 9.963 total
- */
+static inline void trans(int N, unsigned long A[][2048]) {
+    int x;
+    for (int i = 0; i < N; i++) {
+        for (int j = i+1; j < N; j++) {
+            x = A[i][j];
+            A[i][j] = A[j][i];
+            A[j][i] = x;
+        }
+    }
+}
 
 void multiply(int N, unsigned long A[][2048], unsigned long B[][2048], unsigned long C[][2048]) {
+    trans(N, B);
+    #pragma omp parallel for
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             unsigned long sum = 0;
             for (int k = 0; k < N; k++)
-                sum += A[i][k] * B[k][j];
+                sum += A[i][k] * B[j][k];
             C[i][j] = sum;
         }
     }
