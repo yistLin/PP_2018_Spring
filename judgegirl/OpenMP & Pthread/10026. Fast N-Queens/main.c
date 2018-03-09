9b9e9ed7429232e40d79fd1d95162f0becfaf8b8
yistLin <yishen992@gmail.com>
2018-03-09 10:03:19 +0800

    solve with the very basic recursive way

--- judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
+++ judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
-#include <omp.h>
+// #include <omp.h>
+
+#define MAXN 20
 
 int N = 0;
-char board[20][20];
+char board[MAXN][MAXN];
 
-int solve(int row, int state[20]) {
-    if (row == N) {
-        printf("row = %d, the end.\n", N);
+int solve(int row, int state[MAXN]) {
+    if (row == N)
         return 1;
-    }
 
     int nb_answers = 0;
     for (int i = 0; i < N; i++) {
         if (board[row][i] == '*')
             continue;
+        
+        // detect collisions
+        int r = 0;
+        for (; r < row; r++) {
+            if (state[r] == i)  // column
+                break;
+            if ((r - state[r]) == (row - i))  // diagonal
+                break;
+            if ((r + state[r]) == (row + i))  // inverse diagonal
+                break;
+        }
+
+        if (r == row) {  // no collision with previous queens
+            state[row] = i;
+            nb_answers += solve(row+1, state);
+        }
     }
+
+    return nb_answers;
+}
+
+int solve_n_queens() {
+    int state[MAXN];
+    return solve(0, state);
 }
 
 int main(int argc, char* argv[]) {
@@ -27,7 +50,7 @@ int main(int argc, char* argv[]) {
             for (int j = 0; j < N; j++)
                 scanf(" %c", &board[i][j]);
 
-        int nb_answers = 0;
+        int nb_answers = solve_n_queens();
         printf("Case %d: %d\n", case_cnt, nb_answers);
     }
     return 0;
