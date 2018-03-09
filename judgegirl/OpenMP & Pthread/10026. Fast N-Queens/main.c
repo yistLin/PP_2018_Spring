ac48f888f60951441003a12020c1c72ecd54c694
yistLin <yishen992@gmail.com>
2018-03-09 08:58:44 +0800

    temp edit

--- judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
+++ judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <omp.h>
 
+int N = 0;
+char board[20][20];
+
+int solve(int row, int state[20]) {
+    if (row == N) {
+        printf("row = %d, the end.\n", N);
+        return 1;
+    }
+
+    int nb_answers = 0;
+    for (int i = 0; i < N; i++) {
+        if (board[row][i] == '*')
+            continue;
+    }
+}
+
 int main(int argc, char* argv[]) {
-    char board[20][20];
-    int N, case_cnt = 0, nb_answers = 0;
+    int case_cnt = 0;
 
     while (scanf("%d", &N) == 1) {
         case_cnt++;
@@ -12,6 +27,7 @@ int main(int argc, char* argv[]) {
             for (int j = 0; j < N; j++)
                 scanf(" %c", &board[i][j]);
 
+        int nb_answers = 0;
         printf("Case %d: %d\n", case_cnt, nb_answers);
     }
     return 0;
