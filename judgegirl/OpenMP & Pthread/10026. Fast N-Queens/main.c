a1efaaa54a8ec816339978f9b4f349476223cdb0
yistLin <yishen992@gmail.com>
2018-03-17 12:31:59 +0800

    got 60/100 in 10026, not yet AC

--- judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
+++ judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
@@ -6,32 +6,20 @@
 int N = 0;
 char board[MAXN][MAXN];
 
-omp_lock_t writelock;
-
-int solve(int row, int state[MAXN]) {
+int solve(int row, int col[MAXN], int diag[2 * MAXN - 1], int rdiag[2 * MAXN - 1]) {
     if (row == N)
         return 1;
 
     int nb_answers = 0;
-    int candi[MAXN];
-    for (int i = 0; i < N; i++)
-        candi[i] = 1;
-    for (int r = 0; r < row; r++) {
-        candi[state[r]] = 0;
-        int d1 = row - (r - state[r]);
-        if (d1 >= 0 && d1 < N)
-            candi[d1] = 0;
-        int d2 = (r + state[r]) - row;
-        if (d2 >= 0 && d2 < N)
-            candi[d2] = 0;
-    }
     for (int i = 0; i < N; i++) {
         if (board[row][i] == '*')
             continue;
-        if (candi[i] == 0)
-            continue;
-        state[row] = i;
-        nb_answers += solve(row+1, state);
+        int j = i - row + N - 1, k = row + i;
+        if (col[i] && diag[j] && rdiag[k]) {
+            col[i] = diag[j] = rdiag[k] = 0;
+            nb_answers += solve(row+1, col, diag, rdiag);
+            col[i] = diag[j] = rdiag[k] = 1;
+        }
     }
 
     return nb_answers;
@@ -39,25 +27,29 @@ int solve(int row, int state[MAXN]) {
 
 int solve_n_queens() {
     int nb_answers = 0;
-    int state[MAXN];
+    int col[MAXN], diag[2 * MAXN - 1], rdiag[2 * MAXN - 1];
+    for (int i = 0; i < MAXN; i++)
+        col[i] = 1;
+    for (int i = 0; i < (2 * MAXN - 1); i++)
+        diag[i] = rdiag[i] = 1;
 
-    #pragma omp parallel for private(state)
+    #pragma omp parallel for firstprivate(col, diag, rdiag)
     for (int i = 0; i < N; i++) {
         if (board[0][i] == '*')
             continue;
-        state[0] = i;
-        int ret = solve(1, state);
+        int j = i - 0 + N - 1, k = 0 + i;
+        col[i] = diag[j] = rdiag[k] = 0;
+        int ret = solve(1, col, diag, rdiag);
 
-        omp_set_lock(&writelock);
+        #pragma omp atomic
         nb_answers += ret;
-        omp_unset_lock(&writelock);
     }
+
     return nb_answers;
 }
 
 int main(int argc, char* argv[]) {
     int case_cnt = 0;
-    omp_init_lock(&writelock);
 
     while (scanf("%d", &N) == 1) {
         case_cnt++;
@@ -69,7 +61,5 @@ int main(int argc, char* argv[]) {
         int nb_answers = solve_n_queens();
         printf("Case %d: %d\n", case_cnt, nb_answers);
     }
-
-    omp_destroy_lock(&writelock);
     return 0;
 }
