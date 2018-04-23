47bedab9907e6813a08346c84860207b43d85970
yistLin <yishen992@gmail.com>
2018-04-23 23:39:58 +0800

    refactoring

--- judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
+++ judgegirl/OpenMP & Pthread/10026. Fast N-Queens/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <omp.h>
 
 #define MAXN 20
 
-int N = 0;
 char board[MAXN][MAXN];
 
-int solve(int row, long long int col, long long int diag, long long int rdiag) {
+int solve(int N, int row, long long int col, long long int diag, long long int rdiag) {
     if (row == N)
         return 1;
 
@@ -18,50 +16,7 @@ int solve(int row, long long int col, long long int diag, long long int rdiag) {
         if ((col & (1L << i)) &&
             (diag & (1L << j)) &&
             (rdiag & (1L << k))) {
-            nb_answers += solve(row+1, col ^ (1L << i), diag ^ (1L << j), rdiag ^ (1L << k));
-        }
-    }
-
-    return nb_answers;
-}
-
-int solve_n_queens() {
-    int nb_answers = 0;
-
-    #pragma omp parallel for collapse(3) reduction(+:nb_answers) schedule(dynamic)
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                int ret = 0;
-                long long int col, diag, rdiag;
-                col = diag = rdiag = -1L;
-
-                if (board[0][i] == '*' || board[1][j] == '*' || board[2][k] == '*')
-                    continue;
-
-                int a = i - 0 + N - 1, b = 0 + i;
-                col ^= (1L << i);
-                diag ^= (1L << a);
-                rdiag ^= (1L << b);
-
-                int c = j - 1 + N - 1, d = 1 + j;
-                if ((col & (1L << j)) &&
-                    (diag & (1L << c)) &&
-                    (rdiag & (1L << d))) {
-                    col ^= (1L << j);
-                    diag ^= (1L << c);
-                    rdiag ^= (1L << d);
-
-                    int e = k - 2 + N - 1, f = 2 + k;
-                    if ((col & (1L << k)) &&
-                        (diag & (1L << e)) &&
-                        (rdiag & (1L << f))) {
-                        ret = solve(3, col ^ (1L << k), diag ^ (1L << e), rdiag ^ (1L << f));
-                    }
-                }
-
-                nb_answers += ret;
-            }
+            nb_answers += solve(N, row+1, col ^ (1L << i), diag ^ (1L << j), rdiag ^ (1L << k));
         }
     }
 
@@ -69,16 +24,34 @@ int solve_n_queens() {
 }
 
 int main(int argc, char* argv[]) {
-    int case_cnt = 0;
+    int N, case_cnt = 0;
 
     while (scanf("%d", &N) == 1) {
         case_cnt++;
 
+        // input
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
                 scanf(" %c", &board[i][j]);
 
-        int nb_answers = solve_n_queens();
+        int ret = 0, nb_answers = 0;
+        long long int col, diag, rdiag;
+        col = diag = rdiag = -1L;
+
+        // solve
+        #pragma omp parallel for firstprivate(ret, col, diag, rdiag) reduction(+: nb_answers)
+        for (int i = 0; i < N; i++) {
+            if (board[0][i] == '*')
+                continue;
+            int a = i - 0 + N - 1, b = 0 + i;
+            if ((col & (1L << i)) &&
+                (diag & (1L << a)) &&
+                (rdiag & (1L << b))) {
+                ret = solve(N, 1, col ^ (1L << i), diag ^ (1L << a), rdiag ^ (1L << b));
+            }
+            nb_answers += ret;
+        }
+
         printf("Case %d: %d\n", case_cnt, nb_answers);
     }
     return 0;
