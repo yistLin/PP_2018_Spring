6c61f7f35869e2f3b1d92aa69f17f236c3d2bea4
yistLin <yishen992@gmail.com>
2018-04-26 00:44:43 +0800

    10086 improved, 316ms

--- judgegirl/OpenMP & Pthread/10086. Red Blue Computation/main.c
+++ judgegirl/OpenMP & Pthread/10086. Red Blue Computation/main.c
@@ -6,46 +6,6 @@
 
 char grids[2][MAXN][MAXN];
 
-void move_red(const int N) {
-    #pragma omp parallel
-    {
-        #pragma omp for
-        for (int i = 0; i < N; i++) {
-            memcpy((void *)&(grids[1][i][0]), (const void *)&(grids[0][i][0]), N);
-        }
-        #pragma omp for
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                int jj = CLIP(j+1, N);
-                if (grids[0][i][j] == 'R' && grids[0][i][jj] == 'W') {
-                    grids[1][i][j] = 'W';
-                    grids[1][i][jj] = 'R';
-                }
-            }
-        }
-    }
-}
-
-void move_blue(const int N) {
-    #pragma omp parallel
-    {
-        #pragma omp for
-        for (int i = 0; i < N; i++) {
-            memcpy((void *)&(grids[0][i][0]), (const void *)&(grids[1][i][0]), N);
-        }
-        #pragma omp for
-        for (int i = 0; i < N; i++) {
-            int ii = CLIP(i+1, N);
-            for (int j = 0; j < N; j++) {
-                if (grids[1][i][j] == 'B' && grids[1][ii][j] == 'W') {
-                    grids[0][i][j] = 'W';
-                    grids[0][ii][j] = 'B';
-                }
-            }
-        }
-    }
-}
-
 int main(int argc, char *argv[]) {
     int N, M;
     scanf("%d %d", &N, &M);
@@ -54,8 +14,37 @@ int main(int argc, char *argv[]) {
         scanf("%s", grids[0][i]);
 
     for (int i = 0; i < M; i++) {
-        move_red(N);
-        move_blue(N);
+        #pragma omp parallel
+        {
+            #pragma omp for
+            for (int i = 0; i < N; i++) {
+                memcpy((void *)&(grids[1][i][0]), (const void *)&(grids[0][i][0]), N);
+            }
+            #pragma omp for
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    int jj = CLIP(j+1, N);
+                    if (grids[0][i][j] == 'R' && grids[0][i][jj] == 'W') {
+                        grids[1][i][j] = 'W';
+                        grids[1][i][jj] = 'R';
+                    }
+                }
+            }
+            #pragma omp for
+            for (int i = 0; i < N; i++) {
+                memcpy((void *)&(grids[0][i][0]), (const void *)&(grids[1][i][0]), N);
+            }
+            #pragma omp for
+            for (int i = 0; i < N; i++) {
+                int ii = CLIP(i+1, N);
+                for (int j = 0; j < N; j++) {
+                    if (grids[1][i][j] == 'B' && grids[1][ii][j] == 'W') {
+                        grids[0][i][j] = 'W';
+                        grids[0][ii][j] = 'B';
+                    }
+                }
+            }
+        }
     }
 
     for (int i = 0; i < N; i++)
