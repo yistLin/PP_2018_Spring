5cc1676971d18abee2439459d2a2f15f77123468
yistLin <yishen992@gmail.com>
2018-05-11 22:10:19 +0800

    10203 fail, got 75/100 only

--- judgegirl/OpenMP & Pthread/10203. Fast Matrix Chain Multiplication Basic (OpenMP)/sequence.c
+++ judgegirl/OpenMP & Pthread/10203. Fast Matrix Chain Multiplication Basic (OpenMP)/sequence.c
@@ -13,7 +13,9 @@ int main() {
         for (int i=0; i<N; i++)
             dp[i*N + i] = 0;
 
+        #pragma omp parallel
         for (int i = 1; i < N; i++) {
+            #pragma omp for
             for (int j = 0; j < N-i; j++) {
                 int l = j, r = j+i;
                 long long local = INF;
